01d3de065bc3b1a8919bf0b8c8dbfb959792e218
koitsu884 <koitsu884@hotmail.com>
2019-03-15 16:57:26 +1300

    Update

--- client/src/apis/config/keys_prod.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    weather: process.env.OWM_KEY,
-    time: process.env.WWO_KEY,
-}
--- client/src/apis/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    weather: '77a61f58950e9118f458a797c114319e',
-    time: '576e24c0f31247a48eb214848191403'
-}

6560e4081544d7ed66f593f054d72b7d72bbf483
koitsu884 <koitsu884@hotmail.com>
2019-03-15 16:53:35 +1300

    Prepared to push production

--- server.js
+++ server.js
@@ -66,6 +66,13 @@ app.post("/api/cities", async (req, res) => {
   });
 });
 
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
